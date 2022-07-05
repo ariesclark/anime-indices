c67b6f71c5612a8355db9554ef170721c84182bd
ariesclark <ariesclark@users.noreply.github.com>
2022-07-05 01:43:22 +0000

    Apply automatic changes

--- build/content/2024/index.ts
+++ build/content/2024/index.ts
@@ -2,7 +2,8 @@ import { type Anime } from "../../";
 
 import spring from "./spring.json";
 import unknown from "./unknown.json";
+import winter from "./winter.json";
 
 export const year = 2024;
 // @ts-ignore
-export const season: { spring: Anime[], unknown?: Anime[] } = { spring, unknown };
+export const season: { spring: Anime[], unknown?: Anime[], winter: Anime[] } = { spring, unknown, winter };
